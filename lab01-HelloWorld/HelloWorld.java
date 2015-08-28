52b512a73151eea5dc8aefc61bc210dc0a91929d
tweg97 <taw219@lehigh.edu>
2015-08-28 15:56:35 +0000

    updated HelloWorld

---
+++ lab01-HelloWorld/HelloWorld.java
@@ -0,0 +1,24 @@
+//Travis Weghofer
+//Demo 0
+//Hello World Java Program
+//
+//
+//define a class
+public class HelloWorld{
+    
+//  add a main method
+  public static void main(String[] args) {
+      
+//      print the statement with the box around it
+    System.out.println("#######################");
+    System.out.println("#######################");
+    System.out.println("#                     #");
+    System.out.println("#  Hello World 2015!! #");
+    System.out.println("#                     #");
+    System.out.println("#######################");
+    System.out.println("#######################");
+    System.out.println("test");
+        
+//      test/testtest
+  }
+}
