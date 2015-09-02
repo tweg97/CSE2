d8f5e2b943f20e6723b6fe1148a02e040b7a30dd
tweg97 <taw219@lehigh.edu>
2015-09-02 12:12:29 +0000

    updated WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,18 @@
+public class WelcomeClass{
+    
+
+  public static void main(String[] args) {
+      
+
+    System.out.println(" ----------- ");
+    System.out.println(" | WELCOME | ");
+    System.out.println(" ----------- ");
+    System.out.println(" ^  ^  ^  ^  ^  ^ ");
+    System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\ ");
+    System.out.println("<-T--A--W--2--1--9->");
+    System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ / ");
+    System.out.println(" v  v  v  v  v  v ");
+        
+
+  }
+}
