c1a36e182e9440f90a6bcb0d407821203d350ac4
tweg97 <taw219@lehigh.edu>
2015-09-18 02:36:33 +0000

    added comments to Block

--- hw03/Block.java
+++ hw03/Block.java
@@ -7,17 +7,17 @@ public class Block{
         Scanner dScanner = new Scanner ( System.in );
         ///////////////////////
         
-            
+            //enter dimensions
             System.out.print("Enter the length of the block: ");
                 double length = dScanner.nextDouble();
             System.out.print("Enter the width of the block: ");
                 double width = dScanner.nextDouble();
             System.out.print("Enter the height of the block: ");
                 double height = dScanner.nextDouble();
-                    
+                    //calculate volume and surface area
                     double volume = (length * width * height);
                     double sArea = ((2 * length * width) + (2 * length * height) + (2 * width * height));
-                    
+                    //print data
             System.out.println("The volume of the block of dimensions " + length + " x " + width + " x " + height + " is " + volume + ". The surface area of the block is " + sArea + ".");
 
         
